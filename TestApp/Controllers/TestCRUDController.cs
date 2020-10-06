e13ec10f1673659a97d75f0edd9069ab3a716886
Kosobokova <48236986+Kosobokova@users.noreply.github.com>
2020-10-06 15:00:56 +0300

    тестовое задание

---
+++ TestApp/Controllers/TestCRUDController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Linq;
+using TestApp;
+using System.Web.UI.WebControls.WebParts;
+
+namespace TestApp.Controllers
+{
+	public class TestCRUDController : ApiController
+	{
+		public HttpResponseMessage Get()
+		{
+			var result = new HttpResponseMessage();
+			{
+				using (TestDataBase db = new TestDataBase(@"Data Source=192.168.50.7;Initial Catalog=Test;User Id=DB_USER;Password=jlbyldfnhb"))
+				{
+
+					Table<Customers> cust = db.Customers;
+					var toresp = cust.ToList();
+					result = Request.CreateResponse(HttpStatusCode.OK, toresp);
+					var res = result.Content;
+				}
+			};
+			return result;
+		}
+
+		public void Post([FromBody] Customers newcustomer)
+		{
+			Customers cust = newcustomer;
+			using (TestDataBase db = new TestDataBase(@"Data Source=192.168.50.7;Initial Catalog=Test;User Id=DB_USER;Password=jlbyldfnhb"))
+			{
+				db.GetTable<Customers>().InsertOnSubmit(cust);
+				db.SubmitChanges();
+			}
+		}
+
+		public void Put([FromBody] IEnumerable<Customers> newcustomers)
+		{
+			IEnumerable<Customers> cust = newcustomers;
+			using (TestDataBase db = new TestDataBase(@"Data Source=192.168.50.7;Initial Catalog=Test;User Id=DB_USER;Password=jlbyldfnhb"))
+			{
+				foreach (Customers person in cust)
+				{
+					var customerQuery =
+						from pers in db.Customers
+						select pers;
+
+					foreach (var human in customerQuery)
+					{
+						if (human.Id == person.Id)
+						{
+							human.FirstName = person.FirstName;
+							human.LastName = person.LastName;
+							human.Age = person.Age;
+							human.Email = person.Email;
+							human.Phone = person.Phone;
+						}
+					}
+					db.SubmitChanges();
+				}
+			}
+		}
+
+		public void Delete([FromBody] Customers newcustomer)
+		{
+			Customers cust = newcustomer;
+			using (TestDataBase db = new TestDataBase(@"Data Source=192.168.50.7;Initial Catalog=Test;User Id=DB_USER;Password=jlbyldfnhb"))
+			{
+				var customerQuery =
+						from pers in db.Customers
+						where pers.Id == cust.Id
+						select pers;
+
+				foreach (var human in customerQuery)
+				{
+					db.Customers.DeleteOnSubmit(human);
+				}
+
+				//db.GetTable<Customers>().DeleteOnSubmit(cust);
+				db.SubmitChanges();
+			}
+		}
+	}
+}
---
+++ TestApp/ForApp/IndexCtrl.js
@@ -0,0 +1,39 @@
+﻿ var custApp = angular.module("custApp", []);
+        custApp.controller("custController", function ($scope, $http) {
+
+            $http({ method: 'GET', url: '/api/TestCRUD' }).
+                then(function success(response) {
+
+                    $scope.customer = response.data;
+                });
+
+            $scope.addItem = function (firstName, lastName, age, email, phone) {
+                $scope.customer.push({ FirstName: firstName, LastName: lastName, Age: age, Email: email, Phone: phone });
+
+                var tosend = { FirstName: firstName, LastName: lastName, Age: age, Email: email, Phone: phone };
+
+                $http({ method: 'POST', url: '/api/TestCRUD', data: JSON.stringify(tosend) }).then(function success(response) {
+                    $scope.response = response.data;
+                })
+            };
+
+            $scope.updateItem = function (cust) {
+                $scope.customer = cust;
+
+                var tosend = cust;
+
+                $http({ method: 'PUT', url: '/api/TestCRUD', data: JSON.stringify(tosend) }).then(function success(response) {
+                    $scope.response = response.data;
+                })
+            };
+
+            $scope.deleteItem = function (item) {
+                var index = $scope.customer.indexOf(item);
+                var tosend = $scope.customer[index];
+                $scope.customer.splice(index, 1); 
+
+                $http({ method: 'delete', url: '/api/TestCRUD', data: angular.toJson(tosend), headers: { 'Content-Type': 'application/json' } }).then(function success(response) {
+                    $scope.response = response.data;
+                })
+            };
+});
